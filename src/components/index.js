ea8dd4674b05ea7abe87402ff09bc9ccf4e1f761
amir-pasalic <amirpasalic144@gmail.com>
2021-05-02 19:37:29 +0200

    React web application

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import AppFooter from '../components/AppFooter';
+import AppHeader from '../components/AppHeader';
+import Courses from '../components/Courses';
+import Course from '../components/Course';
+import Home from '../components/Home';
+import Students from '../components/Students';
+import Student from '../components/Student';
+
+export { AppFooter, AppHeader, Courses, Course, Home, Students, Student }
